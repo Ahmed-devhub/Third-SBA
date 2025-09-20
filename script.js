2bcc008f4a8a9a149a839b25f8a6aa8604f86ccf
Ahmed <ahmedshariif90@gmail.com>
2025-09-19 22:33:38 -0400

    Cache form, input, and task list with getElementById and querySelector

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("Form");
+const taskInputForm = document.getElementById("enterTask");
+const taskListForm = document.querySelector("ul");
+
+console.log(form);
+console.log(taskInputForm);
+console.log(taskListForm);
